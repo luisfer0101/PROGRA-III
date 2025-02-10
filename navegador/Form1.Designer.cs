da951ee9fcaecef8a14ec419709524538d6bd63c
LuisFer_30 <160041210+luisfer0101@users.noreply.github.com>
2025-02-10 09:23:29 -0600

    webview2
    archivo actualizado

--- navegador/Form1.Designer.cs
+++ navegador/Form1.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.addressBar = new System.Windows.Forms.ComboBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // addressBar
+            // 
+            this.addressBar.FormattingEnabled = true;
+            this.addressBar.Items.AddRange(new object[] {
+            "https://music.youtube.com/watch?v=gXLzCTQ76I4&list=RDAMVMSiRJw4f_ufc"});
+            this.addressBar.Location = new System.Drawing.Point(79, 0);
+            this.addressBar.Name = "addressBar";
+            this.addressBar.Size = new System.Drawing.Size(594, 24);
+            this.addressBar.TabIndex = 1;
+            this.addressBar.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(679, 0);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(109, 23);
+            this.goButton.TabIndex = 2;
+            this.goButton.Text = "Buscar ";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.btnBusqueda_Click);
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(0, 31);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(800, 420);
+            this.webView.Source = new System.Uri("https://www.microsoft.com", System.UriKind.Absolute);
+            this.webView.TabIndex = 3;
+            this.webView.ZoomFactor = 1D;
+            this.webView.Click += new System.EventHandler(this.webView21_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.webView);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.addressBar);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ComboBox addressBar;
+        private System.Windows.Forms.Button goButton;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
     }
 }
 
--- navegador/Form1.cs
+++ navegador/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Web.WebView2.Core;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
 
 namespace navegador
 {
@@ -15,6 +18,95 @@ namespace navegador
         public Form1()
         {
             InitializeComponent();
+            this.Resize += new System.EventHandler(this.Form_Resize);
+            webView.NavigationStarting += EnsureHttps;
+            InitializeAsync();
+        }
+        async void InitializeAsync()
+        {
+            await webView.EnsureCoreWebView2Async(null);
+            webView.CoreWebView2.WebMessageReceived += UpdateAddressBar;
+
+            await webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.postMessage(window.document.URL);");
+            await webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync("window.chrome.webview.addEventListener(\'message\', event => alert(event.data));");
+        }
+
+        void UpdateAddressBar(object sender, CoreWebView2WebMessageReceivedEventArgs args)
+        {
+            String uri = args.TryGetWebMessageAsString();
+            addressBar.Text = uri;
+            webView.CoreWebView2.PostWebMessageAsString(uri);
+        }
+
+        void EnsureHttps(object sender, CoreWebView2NavigationStartingEventArgs args)
+        {
+            String uri = args.Uri;
+            if (!uri.StartsWith("https://"))
+            {
+                webView.CoreWebView2.ExecuteScriptAsync($"alert('{uri} is not safe, try an https link')");
+                args.Cancel = true;
+            }
+        }
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            webView.Size = this.ClientSize - new System.Drawing.Size(webView.Location);
+            goButton.Left = this.ClientSize.Width - goButton.Width;
+            addressBar.Width = goButton.Left - addressBar.Left;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnBusqueda_Click(object sender, EventArgs e)
+        {
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                string url = addressBar.Text.Trim();
+                if (!url.Contains("."))
+                {
+                    url = "https://www.google.com/search?q=" + Uri.EscapeDataString(url);
+                }
+                else
+                {
+                   
+                    if (!(url.ToLower().StartsWith("http://") || url.ToLower().StartsWith("https://")))
+                    {
+                        url = "https://" + url;
+                    }
+                }
+
+                webView.CoreWebView2.Navigate(url);
+            }
+        }
+
+
+        private void inicioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+          //  webBrowser1.GoHome();
+        }
+
+        private void haciaAtrásToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+         //   webBrowser1.GoForward();
+        }
+
+        private void haciaAdelanteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           // webBrowser1.GoBack();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           // cmbBuscar.SelectedIndex = 0;
+            //webBrowser1.GoHome();
+        }
+
+        private void webView21_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
